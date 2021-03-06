d274c3d240dbc14c0831fc3f17394b05598de7fa
Raphael77777 <raphaeldarbellay2000@gmail.com>
2021-03-06 15:41:35 +0200

    06.03.2021 - RMS Impl. - RD

--- src/main/java/RiskHandling/RiskManagementSystem.java
+++ src/main/java/RiskHandling/RiskManagementSystem.java
@@ -1,17 +1,51 @@
 package RiskHandling;
 
+import IbAccountDataHandling.TwsThread;
 import com.ib.client.Order;
+import com.ib.client.OrderType;
 
 import java.util.List;
 
 public class RiskManagementSystem {
     public static boolean verifyOrder (List<Order> bracketOrder) {
 
-        //TODO : Implement verification condition for order
-        if (bracketOrder.size() == 3){
-            return true;
+        /* Order Integrity Verification */
+        if (bracketOrder.size() != 3){
+            return false;
         }
 
-        return false;
+        Order parent = bracketOrder.get(0);
+        Order tp = bracketOrder.get(1);
+        Order sl = bracketOrder.get(2);
+
+        if (parent.orderId() != tp.parentId()){
+            return false;
+        }
+
+        if (parent.orderId() != sl.parentId()){
+            return false;
+        }
+
+        if (parent.totalQuantity() != tp.totalQuantity() || parent.totalQuantity() != sl.totalQuantity()){
+            return false;
+        }
+
+        if (parent.orderType() == OrderType.LMT && TwsThread.strategyData.getOrder().equals("Market")){
+            return false;
+        }
+
+        if (parent.orderType() == OrderType.MKT && TwsThread.strategyData.getOrder().equals("Limit")){
+            return false;
+        }
+
+        if (tp.lmtPrice() == 0 || sl.auxPrice() == 0){
+            return false;
+        }
+
+        if (tp.lmtPrice() <= sl.auxPrice()){
+            return false;
+        }
+
+        return true;
     }
 }
